50ef866d7d36ebed8992fcc11b4be85e8d1e97bf
Minh Le <duyminh.le310@gmail.com>
2022-08-04 13:27:30 -0400

    Almost finish OPEN/CLOSE nav

--- components/Header/Logo.js
+++ components/Header/Logo.js
@@ -1,11 +1,38 @@
 import styles from './Logo.module.scss';
 
-export default function Logo() {
+export default function Logo({expanded, 
+    setExpanded,
+    expansionState,
+    setExpansionState,
+    stateMachine}) {
+    
 
     return (
         <section className={styles.logo}>
-            <button>
-                <i className="fa fa-regular fa-life-ring"></i>
+            <button 
+                onClick={
+                    () => {
+                        console.log(expansionState);
+                        setExpansionState(stateMachine.states[expansionState].on.click)
+                        setExpanded(!expanded);                    
+                    }
+                }
+                onMouseOver={() => {
+                    //console.log(`currentState: ${expansionState} -- MOUSE OVER --> ${stateMachine.states[expansionState].on.mouseOver}`);
+                    setExpansionState(stateMachine.states[expansionState].on.mouseOver)
+
+                }}
+                onMouseOut={() => {
+                    //console.log(`currentState: ${expansionState} -- MOUSE OUT --> ${stateMachine.states[expansionState].on.mouseOut}`);
+
+                    setExpansionState(stateMachine.states[expansionState].on.mouseOut);
+                }}
+                >
+                <i 
+                    className="fa fa-regular fa-life-ring"
+                    data-expansion-state={expansionState}
+
+                    />
             </button>
         </section>
     )
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,15 +1,94 @@
+import { useState } from 'react';
+
 import styles from './Header.module.scss';
 import Logo from './Logo';
 import SocialLinks from './SocialLinks';
 import Title from './Title';
 
-export default function Header() {
+const useExpansionState = () => {
+    const [expanded, setExpanded] = useState(false);
 
-    return (
-        <header className={styles.wrapper}>
-            <Logo />
-            <Title />
-            <SocialLinks />
+    const NORMAL = 'NAV-STATUS/NORMAL',
+            READY = 'NAV-STATUS/READY',
+            CLOSED = 'NAV-STATUS/CLOSED',
+            EXPANDED = 'NAV-STATUS/EXPANDED';
+
+    const stateMachine = {
+        initialState: NORMAL,
+        states: {
+            [NORMAL]: {
+                on: {
+                    mouseOver: READY,
+                    mouseOut: NORMAL,
+                    click: NORMAL
+                }
+            },
+
+            [READY]: {
+                on: {
+                    mouseOut: NORMAL,
+                    mouseOver: READY,
+                    click: expanded? CLOSED: EXPANDED
+                }
+            },
+
+            [EXPANDED]: {
+                on: {
+                    mouseOut: NORMAL,
+                    mouseOver: EXPANDED,
+                    click: CLOSED
+                }
+            },
+
+            [CLOSED]: {
+                on: {
+                    mouseOut: NORMAL,
+                    mouseOver: CLOSED,
+                    click: EXPANDED
+                }
+            }
+        }
+
+
+    }
+    
+    const [expansionState, setExpansionState] = useState(stateMachine.initialState);
+
+    return {
+        expanded,
+        setExpanded,
+        expansionState,
+        setExpansionState,
+        stateMachine
+    }
+    
+    
+}
+
+
+export default function Header() {
+    const {expanded,
+        setExpanded,
+        expansionState,
+        setExpansionState,
+        stateMachine} = useExpansionState();
+
+
+    return ( 
+        <header 
+            className={styles.wrapper} 
+            data-expanded={expanded}>
+            <section className={styles.label}>
+                <Logo 
+                    expanded={expanded}
+                    setExpanded={setExpanded}
+                    expansionState={expansionState}
+                    setExpansionState={setExpansionState}
+                    stateMachine={stateMachine}
+                    />
+                <Title />
+                <SocialLinks />
+            </section>
         </header>
     )
 }
