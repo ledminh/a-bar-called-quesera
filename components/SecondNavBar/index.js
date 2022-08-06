bfa1f4d72140fe948fb142ac7fd3c4abc624007d
Minh Le <duyminh.le310@gmail.com>
2022-08-05 22:10:23 -0400

    Finish SecondNavBar

---
+++ components/SecondNavBar/index.js
@@ -0,0 +1,35 @@
+import styles from './SecondNavBar.module.scss';
+
+const Links = [
+    {
+        text: 'Book',
+        link: '/book'
+    },
+    {
+        text: 'Menu',
+        link: '/menu'
+    },
+    {
+        text: 'Contact',
+        link: '/contact'
+    }
+
+]
+
+export default function SecondNavBar() {
+
+
+    return (
+        <nav className={styles.wrapper}>
+            <ul>
+                {
+                    Links.map((l,i) => (
+                        <li key={i}>
+                            <a href={l.link}>{l.text}</a>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,18 @@
 import MainNavBar from '../components/MainNavBar';
 import PageLayout from '../layouts/page';
 
+import SecondNavBar from '../components/SecondNavBar';
+
+import styles from '../styles/Home.module.scss';
+
 export default function Home() {
   return (
     <>
     <PageLayout>
       <MainNavBar/>
-      
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
-      <p>Culpa qui proident laborum ut aute ullamco consequat quis ex fugiat ut aliquip eiusmod. Irure deserunt do ea quis aliqua sunt tempor aliqua. Et et laboris ad excepteur culpa. Aliqua ullamco aliquip pariatur non culpa aute eiusmod mollit ea consequat excepteur irure aliqua.</p>
+      <main className={styles.main}>
+        <SecondNavBar />
+      </main>
     </PageLayout>
       
     </>    
