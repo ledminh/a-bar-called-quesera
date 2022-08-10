3417c617a6a4f9c9fe46140541c041baac8b0b5e
Minh Le <duyminh.le310@gmail.com>
2022-08-10 18:45:38 -0400

    Finish Banner

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -5,9 +5,12 @@ export default function Banner() {
 
     return (
         <div className={styles.wrapper}>
-            <div className={styles.text}>When you don’t mind the rain, have pancakes in bed or champagne on a Monday?</div>
-            <div className={styles.bullet} />
-            <div className={styles.text}>When you don’t mind the rain, have pancakes in bed or champagne on a Monday?</div>
+            <div className={styles.content}>
+                <div className={styles.text}>When you don’t mind the rain, have pancakes in bed or champagne on a Monday?</div>
+                <div className={styles.bullet}><i className="fa fa-solid fa-star" /></div>
+                <div className={styles.text}>When you don’t mind the rain, have pancakes in bed or champagne on a Monday?</div>
+            </div>
+
         </div>
     );
 }
