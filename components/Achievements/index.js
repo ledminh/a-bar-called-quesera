4cc7c4dd356f2e24d82aa5d7b97bb8eab9ab2d15
Minh Le <duyminh.le310@gmail.com>
2022-08-10 18:54:19 -0400

    Add Achievements

---
+++ components/Achievements/index.js
@@ -0,0 +1,16 @@
+export default function Achievements() {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.photoWrapper}>
+                <div className={styles.photo}>
+                    { 
+                    // Image here 
+                    }
+                </div>
+            </div>
+            <div className={styles.text}>
+                
+            </div>
+        </div>
+    )
+}
