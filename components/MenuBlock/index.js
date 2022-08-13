5f1e668416be8a65b2ab6ebe6711b02a64a64faa
Minh Le <duyminh.le310@gmail.com>
2022-08-13 16:12:00 -0400

    Add general content to menu

--- components/MenuBlock/index.js
+++ components/MenuBlock/index.js
@@ -1,9 +1,22 @@
-export default function MenuBlock() {
+import styles from './MenuBlock.module.scss';
+
+export default function MenuBlock({data}) {
 
 
     return (
-        <>
-            Hello        
-        </>
+        <div className={styles.wrapper}>
+            <h2 className={styles.title}>{data.name}</h2>
+            <ul>
+                {
+                    data.items.map(item => (
+                        <div key={item.name}>
+                            <div>{item.name}</div>
+                            <div>{item.desc? item.desc: null}</div>
+                            <div>{item.price}</div>
+                        </div>
+                    ))
+                }
+            </ul>
+        </div>        
     );
 }
